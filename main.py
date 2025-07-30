c661a5099e2e3e783c5d45dadb92c1f2a714b252
Le Dinh Hoa <dinhhoa.0701@gmail.com>
2025-07-30 21:26:28 +0700

    07/30

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import pandas as pd
 
 def fetch_data():
     url = "https://dummyjson.com/products"
@@ -10,10 +10,13 @@ def fetch_data():
         response.raise_for_status()
         data = response.json()
 
+        df = pd.read_excel("data.xlsx")
+
         print("Kết nối thành công!")
-        print("Tổng số sản phẩm:", len(data["products"]))
-        print("Sản phẩm đầu tiên:")
-        print(json.dumps(data["products"][0], indent=2, ensure_ascii=False))
+        # print("Tổng số sản phẩm:", len(data["products"]))
+        # print("Sản phẩm đầu tiên:")
+        # print(json.dumps(data["products"][0], indent=2, ensure_ascii=False))
+        print(df)
         return data
     except requests.exceptions.RequestException as e:
         print("Lỗi khi kết nối API:", e)
